088c5f9dcf5edf72d0e31920dd10a736f423cb96
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2021-11-01 14:13:10 -0400

    Create Challenge2.js

---
+++ IASC1P02-2021/Lab8/Challenge2.js
@@ -0,0 +1,18 @@
+//Sets the initial colours of the three elements on page load
+document.getElementById("blue").onclick=mix;
+document.getElementById("green").onclick=mix;
+document.getElementById("red").onclick=mix;
+
+//A function to change colours of elements when called
+function mix(){
+	
+	var first = "#00BB00";
+	var second = "#BB9320";
+	var third = "#AB00FF";
+	
+	document.body.style.color="first";
+	document.body.style.color="second";
+	document.body.style.color="third";
+	return false;
+}
+
