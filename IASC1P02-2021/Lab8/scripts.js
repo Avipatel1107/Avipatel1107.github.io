130fa523c5796db4c7bab6fd464096f3fa6ffa02
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2021-11-01 13:51:43 -0400

    Update scripts.js

--- IASC1P02-2021/Lab8/scripts.js
+++ IASC1P02-2021/Lab8/scripts.js
@@ -1,7 +1,7 @@
 /A Function to printout Hurrah! when called
 
-function pushMe(){
-	document.getElementById("output").innerHTML="Hurrah!";
+function myFunction(){
+	document.getElementById("demo").innerHTML="Hurrah!";
 }
 
 
