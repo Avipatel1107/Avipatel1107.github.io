31868d873b378bf63603f38e97f645d264e3c94d
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2021-11-01 19:10:12 -0400

    Update Challenge2.js

--- IASC1P02-2021/Lab8/Challenge2.js
+++ IASC1P02-2021/Lab8/Challenge2.js
@@ -6,7 +6,7 @@ document.getElementById("red").style.color="red";
 
 //A function to change colours of elements when called
 function mix(){
-	
+
 	document.getElementById("blue").style.color='#00BB00';
 	document.getElementById("green").style.color='#BB9320';
 	document.getElementById("red").style.color='#AB00FF';
