3abfc66e2f39561b6c9cb706f2865671bc071b39
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2021-11-14 15:56:40 -0500

    Update Exercise1.js

--- IASC1P02-2021/Lab10/Exercise1.js
+++ IASC1P02-2021/Lab10/Exercise1.js
@@ -1,27 +1,16 @@
+function math(type) {
 
-function Name(){
-  document.getElementsByName("onLoad").innerHTML = onLoad;
+var num1 = parseInt(document.getElementById("num1").value);
+var num2 = parseInt(document.getElementById("num2").value);
+
+if(type == "add") {
+	document.getElementById("output").innerHTML = num1 + num2;
+} else if (type == "subtract") {
+	document.getElementById("output").innerHTML = num1 - num2;
+} else if (type == "multiply") {
+	document.getElementById("output").innerHTML = num1 * num2;
+} else if (type == "divide") {
+	document.getElementById("output").innerHTML = num1 / num2;
 }
 
-var elementOne = document.getElementById("firstNumber");
-var elementTwo = document.getElementById("secondNumber");
-
-function sum() {
-  var calculation = Number(elementOne.value) + Number(elementTwo.value);
-  document.getElementById("Answer").innerHTML = calculation;
-}
-
-function difference() {
-  var calculation = Number(elementOne.value) - Number(elementTwo.value);
-  document.getElementById("Answer").innerHTML = calculation;
-}
-
-function product() {
-  var calculation = Number(elementOne.value) * Number(elementTwo.value);
-  document.getElementById("Answer").innerHTML = calculation;
-}
-
-function quotient() {
-  var calculation = Number(elementOne.value) / Number(elementTwo.value);
-  document.getElementById("Answer").innerHTML = calculation;
 }
