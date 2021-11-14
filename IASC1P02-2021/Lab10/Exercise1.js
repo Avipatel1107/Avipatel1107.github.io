289def7082b8a4f92751f015e687f28c8153deb8
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2021-11-14 16:06:42 -0500

    Update Exercise1.js

--- IASC1P02-2021/Lab10/Exercise1.js
+++ IASC1P02-2021/Lab10/Exercise1.js
@@ -1,22 +1,22 @@
-function addingMech(){
+function adding(){
   var firstValue = Number(document.getElementById('value1').value);
   var secondValue = Number(document.getElementById('value2').value);
   document.getElementById("outputs").innerHTML =  firstValue + secondValue;
 }
 
-function subtractionMech(){
+function subtraction(){
   var firstValue = Number(document.getElementById('value1').value);
   var secondValue = Number(document.getElementById('value2').value);
   document.getElementById("outputs").innerHTML = firstValue - secondValue;
 }
 
-function multiplicationMech(){
+function multiplication(){
   var firstValue = Number(document.getElementById('value1').value);
   var secondValue = Number(document.getElementById('value2').value);
   document.getElementById("outputs").innerHTML = firstValue * secondValue;
 }
 
-function divisionMech(){
+function division(){
   var firstValue = Number(document.getElementById('value1').value);
   var secondValue = Number(document.getElementById('value2').value);
   document.getElementById("outputs").innerHTML = firstValue / secondValue;
