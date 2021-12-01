9bf28cbe2481c05b74e9b31a383549ad543fd7c7
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2021-12-01 14:38:34 -0500

    Update item.js

--- IASC1P02-2021/Assignment_4/item.js
+++ IASC1P02-2021/Assignment_4/item.js
@@ -1,4 +1,32 @@
+//https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp, accessed Dec 1 2020
+
 function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+function currentTime() {
+  let date = new Date(); 
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
+
+  if(hh == 0){
+      hh = 12;
+  }
+  if(hh > 12){
+      hh = hh - 12;
+      session = "PM";
+   }
+
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+   ss = (ss < 10) ? "0" + ss : ss;
+    
+   let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").innerText = time; 
+  let t = setTimeout(function(){ currentTime() }, 1000);
+}
+currentTime();
