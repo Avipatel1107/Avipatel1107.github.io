934370325030346d21e23f8ec30606d771a739ba
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2022-05-26 19:50:19 -0400

    Update DectoBin.js

--- DecToBin/DectoBin.js
+++ DecToBin/DectoBin.js
@@ -1,40 +1,39 @@
-var Decimal = prompt("Type a Number to convert to Binary. MUST BE LESS THAN OR EQUAL TO '255'!");
-while(Decimal > 255){
-Decimal = prompt("INCORRECT NUMBER MUST BE LESS THAN OR EQUAL TO '255'! Type a Number to conver to Binary.");
+// The visible unit converter
+let visible = "binary";
+// Get the divs 
+const binToDecimal = document.getElementById('binToDecimal');
+const decimalToBin = document.getElementById('decimalToBin');
+const visibleDiv = document.getElementById('visibleDiv');
+// Convert to decimal function
+function convertToDecimal(){
+    // Get the number from input
+    // Store in in array
+    // Reverse the array
+    const arr_numberBinnary = document.getElementById('binary').value.split('');
+    const reverse_arr = arr_numberBinnary.reverse(); 
+
+    // Get the spans and reset the error span
+    const err_span = document.getElementsByClassName('err')[0];
+    err_span.textContent = '';
+    const decimal_res_span = document.getElementById('decimal_res');
+    
+    // Intialize the output variable
+    let output = 0;
+    // For Loop 
+    for (number in reverse_arr){
+        // Convert strings from array to integers
+        const int = parseInt(reverse_arr[number],10);
+        // Check if the format is invalid
+        if(int != 0 && int != 1){
+            // Print the error
+            err_span.textContent = "Invalid format! Use only 1 and 0!";
+            decimal_res_span.textContent = '';
+            return; // Stop the function
+        }
+        // Calculate the output
+        output +=  int*Math.pow(2,number);
+    }
+
+    // Display the result if there is no error
+        decimal_res_span.textContent = output;
 }
-var array = [0,0,0,0,0,0,0,0];
-
-if(Decimal >= 128){
-           array[0] = 1;
-   Decimal = Decimal - 128;
-           };
-if(Decimal >= 64){
-           array[1] = 1;
-  Decimal = Decimal - 64;
-           };
-if(Decimal >= 32){
-           array[2] = 1;
-  Decimal = Decimal - 32;
-           };
-if(Decimal >= 16){
-           array[3] = 1;
-  Decimal = Decimal - 16;
-           };
-if(Decimal >= 8){
-           array[4] = 1;
-  Decimal = Decimal - 8;
-           };
-if(Decimal >= 4){
-           array[5] = 1;
-  Decimal = Decimal - 4;
-           };
-if(Decimal >= 2){
-           array[6] = 1;
-  Decimal = Decimal - 2;
-           };
-if(Decimal >= 1){
-           array[7] = 1;
-  Decimal = Decimal - 1;
-           };
-
-document.write(array);
