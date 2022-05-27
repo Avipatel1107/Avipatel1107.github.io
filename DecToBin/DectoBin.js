d73519641cf789010e3157d03cb7a1a4b4e387d8
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2022-05-26 20:02:45 -0400

    Update DectoBin.js

--- DecToBin/DectoBin.js
+++ DecToBin/DectoBin.js
@@ -38,3 +38,56 @@ function convertToDecimal(){
         decimal_res_span.textContent = output;
 }
 
+// Convert to binary function
+function convertToBinary(){
+    // Get the number in decimal
+    const numberDecimal = document.getElementById('decimal').value;
+
+    // Get the spans and reset the error span
+    const err_span = document.getElementsByClassName('err')[0];
+    err_span.textContent = '';
+    const binary_res_span = document.getElementById('binary_res');
+
+    // Create a new regex and check if the regex is valid
+    const regex = new RegExp("^[0-9]*$");
+    const regex_test = regex.test(numberDecimal);
+    // If the regex is invalid print the the error
+    if(!regex_test){
+        err_span.textContent = "Invalid format! Please use only digits!";
+        binary_res_span.textContent = ''; 
+        return;
+    }
+
+    // Intialize output variable, quotien and remainder
+    let output = '';
+    let quotient = numberDecimal;
+    let remainder;
+    // Check if the q is higher than zero
+    while(quotient > 0){
+        // Check if the remainder is divisible by 2
+        remainder = quotient % 2;
+        // Divide the q
+        quotient = parseInt(quotient/2);
+        // Add the values to the output
+        if(remainder == 1){
+            output = (1)+output;
+        } else {
+            output = (0)+output;
+        }
+    }
+
+    // Show the result
+    binary_res_span.textContent = output;
+}
+
+// Change the convert units
+function changeDiv(){
+    if(visible =="binary"){
+        visibleDiv.innerHTML  = decimalToBin.innerHTML;
+        visible = "decimal";
+    } else {
+        visibleDiv.innerHTML  = binToDecimal.innerHTML;
+        visible = "binary";
+    }
+}
+changeDiv();
