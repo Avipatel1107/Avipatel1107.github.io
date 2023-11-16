480af02ae11c4587b862bc5951bc775474eccf9f
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2023-11-16 18:05:55 -0500

    Update

--- Cosc3P95/Assignment_2/autoClient.py
+++ Cosc3P95/Assignment_2/autoClient.py
@@ -3,49 +3,60 @@ import os
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor,SimpleSpanProcessor,ConsoleSpanExporter
 from opentelemetry.sdk.trace.sampling import TraceIdRatioBased
 from opentelemetry.sdk.resources import Resource
 
-# Set up Jaeger exporter
+
+# set up Probability (40%) sampler for the client
+probability_sampler = TraceIdRatioBased(0.4)
+
+# set the sampler onto the global tracer provider
+trace.set_tracer_provider(TracerProvider(sampler=probability_sampler))
+tracer = trace.get_tracer(__name__)
+
+# Use Jaeger exporter alongside Console exporter for local testing
 jaeger_exporter = JaegerExporter(
     agent_host_name="localhost",
     agent_port=6831,
 )
 
-# Set up the tracer provider with the Jaeger exporter
-tracer_provider = TracerProvider(resource=Resource.create({'service.name': 'client'}))
-tracer_provider.add_span_processor(BatchSpanProcessor(jaeger_exporter))
-trace.set_tracer_provider(tracer_provider)
+# set up an exporter for sampled spans
+trace.get_tracer_provider().add_span_processor(
+    SimpleSpanProcessor(ConsoleSpanExporter())
+)
 
-# Sample decision: Probability sampling below 40%
-tracer_provider.sampler = TraceIdRatioBased(0.4)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
 
 def receive_file(file_name):
+    with tracer.start_as_current_span("send_file"):
+
     # Set the server IP address and port
-    server_ip = "127.0.0.1"
-    server_port = 7777
+        server_ip = "127.0.0.1"
+        server_port = 7777
 
-    # Create a client socket
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Create a client socket
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Connect to the server
-    client.connect((server_ip, server_port))
+        # Connect to the server
+        client.connect((server_ip, server_port))
 
-    # Send the file name to the server
-    client.send(file_name.encode())
+        # Send the file name to the server
+        client.send(file_name.encode())
 
-    # Specify the directory where you want to save the files
-    client_output_dir = "./../Assign_2/Server_files/"
+        # Specify the directory where you want to save the files
+        client_output_dir = "./../Assign_2/Server_files/"
 
-    # Create the directory if it doesn't exist
-    os.makedirs(client_output_dir, exist_ok=True)
+        # Create the directory if it doesn't exist
+        os.makedirs(client_output_dir, exist_ok=True)
 
-    # Create the full path for the output file
-    output_file_path = os.path.join(client_output_dir, os.path.basename(file_name))
+        # Create the full path for the output file
+        output_file_path = os.path.join(client_output_dir, os.path.basename(file_name))
 
-    # Receive the file data from the server
-    received_data = client.recv(1024)
+        # Receive the file data from the server
+        received_data = client.recv(1024)
 
     with open(output_file_path, 'wb') as file:
         while received_data:
@@ -55,7 +66,6 @@ def receive_file(file_name):
 
     # Close the client socket
     client.close()
-
 def main():
     # Specifying the directory containing the files we want to transfer
     client_files_dir = "./../Assign_2/client_files/"
--- Cosc3P95/Assignment_2/autoServer.py
+++ Cosc3P95/Assignment_2/autoServer.py
@@ -2,34 +2,60 @@ import socket
 import threading
 import os
 from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor,SimpleSpanProcessor,ConsoleSpanExporter
+from opentelemetry.sdk.trace.sampling import TraceIdRatioBased
 
+# set up alwaysOn sampler for the server
+alwaysOn_sampler = TraceIdRatioBased(1)
+
+# set the sampler onto the global tracer provider
+trace.set_tracer_provider(TracerProvider(sampler=alwaysOn_sampler))
+tracer = trace.get_tracer(__name__)
+
+# Use Jaeger exporter alongside Console exporter for local testing
+jaeger_exporter = JaegerExporter(
+    agent_host_name="localhost",
+    agent_port=6831,
+)
+
+# set up an exporter for sampled spans
+trace.get_tracer_provider().add_span_processor(
+    SimpleSpanProcessor(ConsoleSpanExporter())
+)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
 
 def handle_client(client_socket):
-    # Receive the file name from the client
-    file_name = client_socket.recv(1024).decode()
+    with tracer.start_as_current_span("handle_client"):
 
-    # Specify the server directory where files will be saved
-    server_directory = "./Assign_2/Server_files/"
-    file_path = os.path.join(server_directory, os.path.basename(file_name))
+        # Receive the file name from the client
+        file_name = client_socket.recv(1024).decode()
 
-    # Start a new span for the file handling operation
-    with trace.get_tracer(__name__).start_as_current_span("handle_client"):
-        # Check if the file exists
-        if os.path.isfile(file_path):
-            # Open and read the file in binary mode
-            with open(file_path, 'rb') as file:
-                # Read the file contents
-                file_data = file.read(1024)
-                while file_data:
-                    # Send the file data to the client
-                    client_socket.send(file_data)
+        # Specify the server directory where files will be saved
+        server_directory = "./Assign_2/Server_files/"
+        file_path = os.path.join(server_directory, os.path.basename(file_name))
+
+        # Start a new span for the file handling operation
+        with trace.get_tracer(__name__).start_as_current_span("handle_client"):
+            # Check if the file exists
+            if os.path.isfile(file_path):
+                # Open and read the file in binary mode
+                with open(file_path, 'rb') as file:
+                    # Read the file contents
                     file_data = file.read(1024)
-        else:
-            # If the file does not exist, send an error message
-            client_socket.send("File not found".encode())
+                    while file_data:
+                        # Send the file data to the client
+                        client_socket.send(file_data)
+                        file_data = file.read(1024)
+            else:
+                # If the file does not exist, send an error message
+                client_socket.send("File not found".encode())
 
-    # Close the client socket
-    client_socket.close()
+        # Close the client socket
+        client_socket.close()
 
 def main():
     # Create a tracer for the main function
