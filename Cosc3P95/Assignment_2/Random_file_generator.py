9e14a2523defc9540b659d0df0483065a3105656
Avi Patel <90337579+Avipatel1107@users.noreply.github.com>
2023-11-16 15:33:39 -0500

    Files

---
+++ Cosc3P95/Assignment_2/Random_file_generator.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+
+# Specify the local folder path where you want to create the files.
+folder_path = "/Users/avipatel/desktop/Assign_2/files"
+
+# Function to create a random file of a specified size
+def create_random_file(file_path, size_bytes):
+    with open(file_path, 'wb') as file:
+        file.write(os.urandom(size_bytes))
+
+# Create at least 20 random files with varying sizes
+for i in range(1, 21):
+    # Generate a random size between 5KB and 100MB
+    size_bytes = random.randint(5 * 1024, 100 * 1024 * 1024)
+    file_name = f"file_{i}.txt"
+    file_path = os.path.join(folder_path, file_name)
+    create_random_file(file_path, size_bytes)
+    print(f"Created {file_name} with size {size_bytes / 1024} KB")
+
+print("Files created successfully.")
---
+++ Cosc3P95/Assignment_2/client.py
@@ -0,0 +1,46 @@
+import socket
+import os
+
+
+def send_file(file_name):
+
+    # Set the server IP address and port
+    server_ip = "127.0.0.1"
+    server_port = 9999
+
+    # Create a client socket
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    client.connect((server_ip, server_port))
+
+    # Send the file name to the server
+    client.send(file_name.encode())
+
+    # Receive the file data from the server
+    received_data = client.recv(1024)
+
+    while received_data:
+        # Write the received data to a new file in the server directory
+        with open("./../Assign_2/Server_files/" + os.path.basename(file_name), 'ab') as file:
+            file.write(received_data)
+        received_data = client.recv(1024)
+
+    # Close the client socket
+    client.close()
+
+def main():
+    # Specifying the directory containing the files we want to transfer
+    client_files_dir = "./../Assign_2/client_files/"
+
+    # Iterate over each file in the directory
+    for file_name in os.listdir(client_files_dir):
+        file_path = os.path.join(client_files_dir, file_name)
+
+        # Check if it's a file (not a directory)
+        if os.path.isfile(file_path):
+            # Call the send_file function with the file name
+            send_file(file_path)
+
+if __name__ == "__main__":
+    main()
